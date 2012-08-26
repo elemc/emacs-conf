e8f56941b0476641074f3e39dbc708f725bfee95
elemc <me@elemc.name>
2012-08-26 15:12:23 +0400

    Move slime only for linux

--- emacs.d/conf/70-slime.el
+++ emacs.d/conf/70-slime.el
@@ -1,5 +1,8 @@
 ;; Set up the Common Lisp environment
 ;; (add-to-list 'load-path "/usr/share/common-lisp/source/slime/")
-(setq inferior-lisp-program "/usr/bin/sbcl")
-(require 'slime)
-(slime-setup)
+(if (eq system-type 'gnu/linux)
+    (progn
+
+      (setq inferior-lisp-program "/usr/bin/sbcl")
+      (require 'slime)
+      (slime-setup)))
