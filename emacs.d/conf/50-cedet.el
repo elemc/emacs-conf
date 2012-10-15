22b77b1be2e7993feea50365fbbbb5850dc5ffc1
Alexei Panov <me@elemc.name>
2012-10-16 02:49:26 +0400

    Update emacs.d/conf/50-cedet.el

--- emacs.d/conf/50-cedet.el
+++ emacs.d/conf/50-cedet.el
@@ -1,11 +1,4 @@
-;;; emacs-rc-cedet.el ---
-
-;; Copyright (C) 2003 Alex Ott
-;;
-;; Author: alexott@gmail.com
-;; Keywords:
-;; Requirements:
-;; Status: not intended to be distributed yet
+;; cedet
 
 (semantic-load-enable-excessive-code-helpers)
 
